0aa24e0ce54a9e559e68d6f2d1b7a277b297ea20
Carlos Santana Roldán <carlos@milkzoft.com>
2015-09-29 16:23:14 -0700

    Adding gulp tasks

---
+++ gulp/tasks/browserify.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+
+module.exports = function() {
+    return browserify('./examples/source/app.js')
+        .transform(babelify)
+        .bundle()
+        .pipe(source('app.js'))
+        .pipe(gulp.dest('./examples/build/'));
+};
---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+var gulp = require('./gulp')([
+  'browserify'
+]);
+
+gulp.task('build', ['browserify']);
+gulp.task('default', ['build']);
