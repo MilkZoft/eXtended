c94256b95984a02b52642245fb07ded1aa5ff662
Carlos Santana Roldán <carlos@milkzoft.com>
2015-09-29 16:24:13 -0700

    Moving example code to examples folder

---
+++ examples/source/app.js
@@ -0,0 +1,23 @@
+(function() {
+  'use strict';
+
+  var eX = require('./../../src/eXtended');
+
+  var h1 = eX.create('h1#myId.myClass', {
+      text: 'Hello World!'
+  });
+
+  var input = eX.create('input', {
+      name: 'user',
+      value: 'Username'
+  });
+
+  var span = eX.create('span', '.username', 'Write your username');
+  var link = eX.create('link', 'public/css/mediaqueries/desktop.css');
+  var script = eX.create('script', 'public/js/script.js');
+
+  var p = eX.create('p#center.other.other2');
+
+  eX.render('head', link, script);
+  eX.render('body', h1, input, span, p);
+})();
