545c0880b3718a80dc3a4189e7df10549a35ecb0
Carlos Santana Roldán <carlos@milkzoft.com>
2015-10-03 19:58:17 -0700

    Moving anonymous functions to ES6 anonymous functions

--- test/eXtendedTest.js
+++ test/eXtendedTest.js
@@ -2,21 +2,21 @@
 
 var eXtended = require('../src/eXtended');
 
-describe('eXtended Library', function() {
-  describe('#create', function() {
-    it('should be a function', function() {
+describe('eXtended Library', () => {
+  describe('#create', () => {
+    it('should be a function', () => {
       assert.typeOf(eXtended.create, 'function', 'create should be a function');
     });
   });
 
-  describe('#element', function() {
-    it('should be a function', function() {
+  describe('#element', () => {
+    it('should be a function', () => {
       assert.typeOf(eXtended.element, 'function', 'element should be a function');
     });
   });
 
-  describe('#render', function() {
-    it('should be a function', function() {
+  describe('#render', () => {
+    it('should be a function', () => {
       assert.typeOf(eXtended.render, 'function', 'render should be a function');
     });
   });
--- test/lib/utilsTest.js
+++ test/lib/utilsTest.js
@@ -2,135 +2,135 @@
 
 var utils = require('../../src/lib/utils');
 
-describe('Utils', function() {
-  describe('#convertCollectionToArray', function() {
-    it('should be a function', function() {
+describe('Utils', () => {
+  describe('#convertCollectionToArray', () => {
+    it('should be a function', () => {
       assert.typeOf(utils.convertCollectionToArray, 'function', 'convertCollectionToArray should be a function');
     });
   });
 
-  describe('#forEach', function() {
-    it('should be a function', function() {
+  describe('#forEach', () => {
+    it('should be a function', () => {
       assert.typeOf(utils.forEach, 'function', 'forEach should be a function');
     });
   });
 
-  describe('#getDefaultAttrs', function() {
-    it('should be a function', function() {
+  describe('#getDefaultAttrs', () => {
+    it('should be a function', () => {
       assert.typeOf(utils.getDefaultAttrs, 'function', 'getDefaultAttrs should be a function');
     });
   });
 
-  describe('#getElement', function() {
-    it('should be a function', function() {
+  describe('#getElement', () => {
+    it('should be a function', () => {
       assert.typeOf(utils.getElement, 'function', 'getElement should be a function');
     });
   });
 
-  describe('#getElementNameAndType', function() {
-    it('should be a function', function() {
+  describe('#getElementNameAndType', () => {
+    it('should be a function', () => {
       assert.typeOf(utils.getElementNameAndType, 'function', 'getElementNameAndType should be a function');
     });
   });
 
-  describe('#getElementType', function() {
-    it('should be a function', function() {
+  describe('#getElementType', () => {
+    it('should be a function', () => {
       assert.typeOf(utils.getElementType, 'function', 'getElementType should be a function');
     });
   });
 
-  describe('#getProperty', function() {
-    it('should be a function', function() {
+  describe('#getProperty', () => {
+    it('should be a function', () => {
       assert.typeOf(utils.getProperty, 'function', 'getProperty should be a function');
     });
   });
 
-  describe('#getRegex', function() {
-    it('should be a function', function() {
+  describe('#getRegex', () => {
+    it('should be a function', () => {
       assert.typeOf(utils.getRegex, 'function', 'getRegex should be a function');
     });
   });
 
-  describe('#getRegexMatch', function() {
-    it('should be a function', function() {
+  describe('#getRegexMatch', () => {
+    it('should be a function', () => {
       assert.typeOf(utils.getRegexMatch, 'function', 'getRegexMatch should be a function');
     });
   });
 
-  describe('#isArray', function() {
-    it('should be a function', function() {
+  describe('#isArray', () => {
+    it('should be a function', () => {
       assert.typeOf(utils.isArray, 'function', 'isArray should be a function');
     });
   });
 
-  describe('#isDefined', function() {
-    it('should be a function', function() {
+  describe('#isDefined', () => {
+    it('should be a function', () => {
       assert.typeOf(utils.isDefined, 'function', 'isDefined should be a function');
     });
   });
 
-  describe('#isDirective', function() {
-    it('should be a function', function() {
+  describe('#isDirective', () => {
+    it('should be a function', () => {
       assert.typeOf(utils.isDirective, 'function', 'isDirective should be a function');
     });
   });
 
-  describe('#isFunction', function() {
-    it('should be a function', function() {
+  describe('#isFunction', () => {
+    it('should be a function', () => {
       assert.typeOf(utils.isFunction, 'function', 'isFunction should be a function');
     });
   });
 
-  describe('#isIn', function() {
-    it('should be a function', function() {
+  describe('#isIn', () => {
+    it('should be a function', () => {
       assert.typeOf(utils.isIn, 'function', 'isIn should be a function');
     });
   });
 
-  describe('#isInitializedArray', function() {
-    it('should be a function', function() {
+  describe('#isInitializedArray', () => {
+    it('should be a function', () => {
       assert.typeOf(utils.isInitializedArray, 'function', 'isInitializedArray should be a function');
     });
   });
 
-  describe('#isObject', function() {
-    it('should be a function', function() {
+  describe('#isObject', () => {
+    it('should be a function', () => {
       assert.typeOf(utils.isObject, 'function', 'isObject should be a function');
     });
   });
 
-  describe('#isSelfClosingDirective', function() {
-    it('should be a function', function() {
+  describe('#isSelfClosingDirective', () => {
+    it('should be a function', () => {
       assert.typeOf(utils.isSelfClosingDirective, 'function', 'isSelfClosingDirective should be a function');
     });
   });
 
-  describe('#isSpecialTag', function() {
-    it('should be a function', function() {
+  describe('#isSpecialTag', () => {
+    it('should be a function', () => {
       assert.typeOf(utils.isSpecialTag, 'function', 'isSpecialTag should be a function');
     });
   });
 
-  describe('#isString', function() {
-    it('should be a function', function() {
+  describe('#isString', () => {
+    it('should be a function', () => {
       assert.typeOf(utils.isString, 'function', 'isString should be a function');
     });
   });
 
-  describe('#log', function() {
-    it('should be a function', function() {
+  describe('#log', () => {
+    it('should be a function', () => {
       assert.typeOf(utils.log, 'function', 'log should be a function');
     });
   });
 
-  describe('#merge', function() {
-    it('should be a function', function() {
+  describe('#merge', () => {
+    it('should be a function', () => {
       assert.typeOf(utils.merge, 'function', 'merge should be a function');
     });
   });
 
-  describe('#newElement', function() {
-    it('should be a function', function() {
+  describe('#newElement', () => {
+    it('should be a function', () => {
       assert.typeOf(utils.newElement, 'function', 'newElement should be a function');
     });
   });
