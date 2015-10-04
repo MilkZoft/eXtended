1115adfe16819593549749d477532fe1615f7cb8
Carlos Santana Roldán <carlos@milkzoft.com>
2015-10-03 21:53:06 -0700

    Adding more unit tests for utils

--- test/lib/utilsTest.js
+++ test/lib/utilsTest.js
@@ -3,21 +3,68 @@
 var utils = require('../../src/lib/utils');
 
 describe('Utils', () => {
-  describe('#convertCollectionToArray', () => {
-    it('should be a function', () => {
-      assert.typeOf(utils.convertCollectionToArray, 'function', 'convertCollectionToArray should be a function');
-    });
-  });
-
   describe('#forEach', () => {
     it('should be a function', () => {
       assert.typeOf(utils.forEach, 'function', 'forEach should be a function');
+    });
+
+    it('should iterate over an array', () => {
+      var numbers = [1, 2, 3, 4, 5];
+      var actualResult = 0;
+      var expectedResult = 15;
+
+      utils.forEach(numbers, (number) => {
+        actualResult += number;
+      });
+
+      assert.strictEqual(actualResult, expectedResult, 'actualResult should be equal to expectedResult');
+    });
+
+    it('should iterate over an object', () => {
+      var obj = {a: 1, b: 2, c: 3, d: 4, e: 5};
+      var actualResult = 0;
+      var expectedResult = 15;
+
+      utils.forEach(obj, (key, index) => {
+        actualResult += obj[key];
+      });
+
+      assert.strictEqual(actualResult, expectedResult, 'actualResult should be equal to expectedResult');
     });
   });
 
   describe('#getDefaultAttrs', () => {
     it('should be a function', () => {
       assert.typeOf(utils.getDefaultAttrs, 'function', 'getDefaultAttrs should be a function');
+    });
+
+    it('should return default attributes for a link tag', () => {
+      var actualResult = utils.getDefaultAttrs('link', 'foo.css');
+      var expectedResult = {
+        rel: 'stylesheet',
+        type: 'text/css',
+        href:  'foo.css',
+        media: 'all'
+      };
+
+      assert.deepEqual(actualResult, expectedResult, 'actualResult should match expectedResult');
+    });
+
+    it('should return default attributes for a script tag', () => {
+      var actualResult = utils.getDefaultAttrs('script', 'bar.js');
+      var expectedResult = {
+        type: 'application/javascript',
+        src:  'bar.js'
+      };
+
+      assert.deepEqual(actualResult, expectedResult, 'actualResult should match expectedResult');
+    });
+
+    it('should return an empty object if the tag is not a script or link', () => {
+      var actualResult = utils.getDefaultAttrs('div');
+      var expectedResult = {};
+
+      assert.deepEqual(actualResult, expectedResult, 'actualResult should match expectedResult');
     });
   });
 
@@ -115,17 +162,79 @@ describe('Utils', () => {
     it('should be a function', () => {
       assert.typeOf(utils.isString, 'function', 'isString should be a function');
     });
+
+    it('should validate if a string value is a String', () => {
+      var value = 'Foo';
+
+      assert.isTrue(utils.isString(value), 'Foo should be String');
+    });
+
+    it('should validate if a number value is not a String', () => {
+      var value = 9999;
+
+      assert.isFalse(utils.isString(value), 'Number 9999 should not be String');
+    });
+
+    it('should validate if an array is not a String', () => {
+      var value = ['Foo'];
+
+      assert.isFalse(utils.isString(value), 'Array should not be String');
+    });
+
+    it('should validate if an object is not a String', () => {
+      var value = {
+        foo: 'bar'
+      };
+
+      assert.isFalse(utils.isString(value), 'Object should not be String');
+    });
+
+    it('should validate if a function is not a String', () => {
+      var value = () => {
+        return 'Foo';
+      };
+
+      assert.isFalse(utils.isString(value), 'Function should not be String');
+    });
   });
 
   describe('#log', () => {
     it('should be a function', () => {
       assert.typeOf(utils.log, 'function', 'log should be a function');
     });
+
+    it('should log a message when is called', () => {
+      var logSpy = sinon.spy(console, 'log');
+
+      utils.log('Testing log');
+
+      assert.isTrue(logSpy.called, 'message should be logged');
+      logSpy.restore();
+    });
   });
 
   describe('#merge', () => {
     it('should be a function', () => {
       assert.typeOf(utils.merge, 'function', 'merge should be a function');
+    });
+
+    it('should merge two objects', () => {
+      var obj1 = {
+        a: 'foo'
+      };
+
+      var obj2 = {
+        b: 'bar'
+      };
+
+      var expectedObj = {
+        a: 'foo',
+        b: 'bar'
+      };
+
+      var mergedObj = utils.merge(obj1, obj2);
+
+      assert.deepEqual(mergedObj, expectedObj, 'mergedObj should match expectedObj');
     });
   });
 
